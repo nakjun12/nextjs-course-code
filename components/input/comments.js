d418c4aed9cd229bdd8c44fda75a42fe62d60d45
nakjun <wns450@gmail.com>
2023-02-28 21:07:42 +0900

    업로드

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import CommentList from './comment-list';
-import NewComment from './new-comment';
-import classes from './comments.module.css';
+import CommentList from "./comment-list";
+import NewComment from "./new-comment";
+import classes from "./comments.module.css";
 
 function Comments(props) {
   const { eventId } = props;
@@ -14,13 +14,20 @@ function Comments(props) {
   }
 
   function addCommentHandler(commentData) {
+    fetch("/api/comments/" + eventId, {
+      method: "POST",
+      body: JSON.stringify(commentData),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
     // send data to API
   }
 
   return (
     <section className={classes.comments}>
       <button onClick={toggleCommentsHandler}>
-        {showComments ? 'Hide' : 'Show'} Comments
+        {showComments ? "Hide" : "Show"} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
       {showComments && <CommentList />}
